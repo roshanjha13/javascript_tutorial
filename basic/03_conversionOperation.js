67dd9b79ecbb6f1ba2b9bb5b82cfe87a51a06589
Roshan <roshan.jha@hih7.in>
2023-08-23 12:11:27 +0530

    conversion of data types

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "33abc";
+let score = "roshan";
+
+// console.log(typeof score);
+
+// conv string value into number
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc"=>nan type is number
+//true,false => 0,1
+
+let isLoggedIn = "roshan";
+
+let boolenaIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof boolenaIsLoggedIn);
+// console.log(boolenaIsLoggedIn);
+
+// 1=> true; 0=>false
+//"" => true ; "roshan"=>true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
