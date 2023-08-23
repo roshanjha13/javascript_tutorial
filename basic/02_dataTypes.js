c94fc23faee0db21b8b36b8fc99905d6ee5a04d5
Roshan <roshan.jha@hih7.in>
2023-08-23 11:49:46 +0530

    data type and variable

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,9 @@
+let name = "roshan";
+let age = 23;
+let isLoggedIn = false;
+let nullify = null;
+//symbol => unique
+
+//object
+
+console.log(typeof nullify);
---
+++ basic/variable.js
@@ -0,0 +1,14 @@
+const accountId = 1445533;
+let accountEmail = "roshan@gmial.com";
+var accountPassword = "12345";
+accountCity = "Jaipur ";
+
+// accountId = 2;
+
+accountEmail = "hc@hc.com";
+accountPassword = "21121";
+accountCity = "kolkata";
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity]);
