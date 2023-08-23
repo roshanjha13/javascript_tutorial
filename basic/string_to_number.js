fd65b5a85d760a8c18a292bf7d50fded15ecdd88
Roshan <roshan.jha@hih7.in>
2023-08-23 12:28:30 +0530

    string to number conversion

---
+++ basic/string_to_number.js
@@ -0,0 +1,23 @@
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+let str1 = "hello";
+let str2 = " roshan";
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + "2");//12
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(true); // true
+// console.log(+true); //1
+// console.log(true+); //error
+
+let gameCounter = 100;
+// ++gameCounter;
+// console.log(++gameCounter);
+console.log(gameCounter++);
