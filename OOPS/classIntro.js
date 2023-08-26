b14f2239c99651e089ac3a1587cd7204c6e47300
Roshan <roshan.jha@hih7.in>
2023-08-26 18:21:58 +0530

    oops intro

---
+++ OOPS/classIntro.js
@@ -0,0 +1,35 @@
+class Product {
+  discount = 10;
+  constructor(n, p) {
+    //create new real life instance
+    //when we create an object this constructor is the first function thats get created
+    this.name = n; //data member
+    this.price = p;
+  }
+  //member function
+  displayProduct() {
+    console.log(this.name, this.price);
+  }
+
+  buyProduct() {
+    console.log(this.name, this.discount);
+  }
+}
+
+let iphone = new Product("iphone11", "200000");
+let macbook = new Product("macbook", "2000000");
+console.log(iphone);
+console.log(macbook);
+iphone.displayProduct();
+iphone.buyProduct();
+//other example
+//
+// let obj = {
+//   name: "roshan",
+//   age: 23,
+// };
+
+//create object: this is create a new object in product class
+//let iphone = new Product("iphone","2000")
+//first constructor(c) function that called c(p,n) c have 2 parameter
+// class is a wrapper over function
