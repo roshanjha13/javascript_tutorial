d2d97cc195c98edaf8a2e84252dc41bbdf59690e
Roshan <roshan.jha@hih7.in>
2023-09-01 11:20:00 +0530

    Global and local scope

---
+++ scope/01_scope.js
@@ -0,0 +1,15 @@
+// var c = 300;
+
+let a = 200;
+if (true) {
+  let a = 10;
+  const b = 20;
+  console.log(a); //10
+  //   var c = 30;
+} //scope
+
+console.log(a);
+
+// console.log(b);
+
+// console.log(c);
