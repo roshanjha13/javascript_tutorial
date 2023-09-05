032006e3886c37b622582e72d99665857efc1488
Roshan <roshan.jha@hih7.in>
2023-09-05 13:10:28 +0530

    Array of objecta and array in for each

---
+++ Loop/05_ForEachArray.js
@@ -0,0 +1,37 @@
+const coding = ["js", "ruby", "python", "java", "cpp"];
+
+// coding.forEach(function (item) {
+//   console.log(item);
+// });
+
+//pass function on foreach
+
+// function printme(item) {
+//   console.log(item);
+// }
+// coding.forEach(printme);
+
+coding.forEach(function (item, index, arr) {
+  //   console.log(item, index, arr);
+});
+
+//array of object
+
+const myCoding = [
+  {
+    languageName: "javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "java",
+    languageFileName: ".java",
+  },
+  {
+    languageName: "c++",
+    languageFileName: "cpp",
+  },
+];
+
+myCoding.forEach((item) => {
+  console.log(item.languageFileName);
+});
