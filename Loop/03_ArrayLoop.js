d2fade4aa69ec0a2943657ab08959f88308b9b38
Roshan <roshan.jha@hih7.in>
2023-09-05 12:24:02 +0530

    loop over array and map

---
+++ Loop/03_ArrayLoop.js
@@ -0,0 +1,38 @@
+//for of
+
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// for (const num of arr) {
+//   console.log(num);
+// }
+
+console.log("-----------------------------------------------------");
+
+// const greetings = "hello world";
+// for (const greet of greetings) {
+//   console.log(`each char is ${greet}`);
+// }
+
+//maps
+//it should be unique and stored ordered value
+const map = new Map();
+map.set("IN", "INDIA");
+map.set("USA", "UNITED STATES OF AMERICA");
+map.set("FR", "FRANCE");
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//   console.log(key, ":-", value);
+// }
+
+//here we print key and value differently
+
+const myObj = {
+  game1: "nfs",
+  game2: "cricket",
+};
+
+// for (const [key, value] of myObj) {
+//   console.log(key, ":-", value);
+// }
