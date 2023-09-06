18be55c25b928586319caa8e69d2c04a2bff14fc
Roshan <roshan.jha@hih7.in>
2023-09-06 14:49:26 +0530

    reduce update

--- Loop/08_reduce.js
+++ Loop/08_reduce.js
@@ -2,7 +2,14 @@ const myNums = [1, 2, 3];
 
 // const initialValue = 0;
 
-const myTotal = myNums.reduce(function (acc, crrVal) {
+// const myTotal = myNums.reduce(function (acc, crrVal) {
+//   console.log(`acc:${acc} and currval:${crrVal}`);
+//   return acc + crrVal;
+// }, 3);
+// console.log(myTotal);
+
+const myTotal = myNums.reduce((acc, crrVal) => {
+  console.log(`acc:${acc} and currval:${crrVal}`);
   return acc + crrVal;
-}, 0);
+}, 3);
 console.log(myTotal);
