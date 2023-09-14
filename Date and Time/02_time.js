0af762f3ae74af79eef13a800a0515d47b29ce90
Roshan <roshan.jha@hih7.in>
2023-09-14 13:04:18 +0530

    time in javascript

---
+++ Date and Time/02_time.js
@@ -0,0 +1,25 @@
+let myCreateDate = new Date("01-14-2023");
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// 1694676199777;
+// console.log(myCreateDate.getTime());
+// 1673634600000;
+// all value give in miliseconds
+
+//convert in milisec to seconds
+console.log(Math.floor(Date.now() / 1000));
+
+// comparison always in miliseconds
+
+let newDate = new Date();
+// console.log(newDate.getDate());
+14;
+console.log(newDate.getDay());
+// 4;Gets the day of the week, using local time
+// console.log(newDate.getFullYear());
+// 2023;
+// console.log(newDate.getHours());
+// 13;
+// console.log(newDate.getTime());
+// 1694676605767;
